c6cadecf619bcfa9d852318c38d2e5285eeff106
lebakammabatho <lebakammabatho@gmail.com>
2023-07-17 13:26:12 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,9 @@
-#include<stdlib.h>
-#include<time.h>
-#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
 /**
- * main - Determines if a umber is positive, negative or zero.
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 
@@ -12,13 +12,13 @@ int main(void)
         int n;
         
         srand(time(0));
-        n=rand() - RAND_MAX/2;
-        if(n>0)
-                printf("%d is positive\n",n);
+        n = rand() - RAND_MAX / 2;
+        if (n>0)
+                printf("%d is positive\n", n);
         else if (n == 0)
-                printf("%d is zero\n",n);
+                printf("%d is zero\n", n);
         else
-                printf("%d is negative\n",n);
+                printf("%d is negative\n", n);
         return (0);
 }
 
